9947e4c44d6574a015117170c703d7d803818837
Chris Sewelll <chrisj_sewell@hotmail.com>
2015-06-14 04:58:01 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 import mock
 
-MOCK_MODULES = ['cclib', 'chemlab', 'chemview']
+MOCK_MODULES = ['cclib', 'chemlab', 'chemview', 'numpy', 'matplotlib']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
     

bedcaa102a93f17798ae71b215bf9138bb49e2e0
Chris Sewell <chrisj_sewell@hotmail.com>
2015-06-15 11:57:26 +0100

    shortened analysis tests
    less molecules

--- pygauss/tests/test_analysis.py
+++ pygauss/tests/test_analysis.py
@@ -22,7 +22,7 @@ class Test_Analysis(object):
         analysis = pg.Analysis(folder_obj=self.folder)
         analysis.add_runs(headers=['Cation', 'Anion', 'Initial'], 
                                        values=[['emim'], ['cl'],
-                                               ['B', 'BE', 'BM', 'F', 'FE']],
+                                               ['B', 'F', 'FE']],
                     init_pattern='*{0}-{1}_{2}_init.com',
                     opt_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_opt*unfrz.log',
                     freq_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_freq*.log',
@@ -35,13 +35,13 @@ class Test_Analysis(object):
         analysis = pg.Analysis(folder_obj=self.folder)
         analysis.add_runs(headers=['Cation', 'Anion', 'Initial'], 
                                        values=[['emim'], ['cl'],
-                                               ['B', 'BE', 'BM', 'F', 'FE']],
+                                               ['B', 'F', 'FE']],
                     init_pattern='*{0}-{1}_{2}_init.com',
                     opt_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_opt*unfrz.log',
                     freq_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_freq*.log',
                     nbo_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_pop-nbo-full-*.log',
                     alignto=[3,2,1], atom_groups={'emim':range(20), 'cl':[20]})
 
-        fig, caption = analysis.plot_mol_images(mtype=mtype, max_cols=3,
+        fig, caption = analysis.plot_mol_images(mtype=mtype, max_cols=2,
                                 info_columns=['Cation', 'Anion', 'Initial'],
                                 rotations=[[0,0,90]])

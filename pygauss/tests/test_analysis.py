9c39d5f676133913c3691f1ecc6681e330d1ecd3
chrisjsewell <cjs14@ch-mint.ch.ic.ac.uk>
2015-06-15 14:51:56 +0100

    update

--- pygauss/tests/test_analysis.py
+++ pygauss/tests/test_analysis.py
@@ -6,7 +6,7 @@ Created on Mon Jun 15 10:55:57 2015
 """
 
 from nose import tools as that
-from nose_parameterized import parameterized
+from nose_parameterized import parameterized, param
 
 import pygauss as pg
 
@@ -28,6 +28,28 @@ class Test_Analysis(object):
                     freq_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_freq*.log',
                     nbo_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_pop-nbo-full-*.log',
                     alignto=[3,2,1], atom_groups={'emim':range(20), 'cl':[20]})
+
+    @parameterized([param('name','get_optimisation_E'),
+                    param('name','calc_bond_angle', [1, 4, 9]),
+                    param('name','calc_dihedral_angle', [1, 4, 9, 10]),
+                    param('name','calc_min_dist', 'emim', 'cl'),
+                    param('name','calc_2plane_angle', [1,2,3], [8,9,10]),
+                    param('name','calc_nbo_charge', 'emim'),
+                    param('name', 'calc_hbond_energy'),
+                    param(['name', 'name', 'name'], 'calc_polar_coords_from_plane', 3, 2, 1, 20)
+                  ])
+    def test_add_mol_property(self, name, prop, *args, **kwargs):
+        analysis = pg.Analysis(folder_obj=self.folder)
+        analysis.add_runs(headers=['Cation', 'Anion', 'Initial'],
+                          values=[['emim'], ['cl'],
+                                  ['B', 'F', 'FE']],
+                          init_pattern='*{0}-{1}_{2}_init.com',
+                          opt_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_opt*unfrz.log',
+                          freq_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_freq*.log',
+                          nbo_pattern='*{0}-{1}_{2}_6-311+g-d-p-_gd3bj_pop-nbo-full-*.log',
+                          alignto=[3,2,1], atom_groups={'emim':range(20), 'cl':[20]})
+
+        analysis.add_mol_property(name, prop, *args, **kwargs)
 
     @parameterized(['initial', 'optimised', 'highlight', 'nbo', 'sopt', 'hbond'])
     def test_tbl_images(self, mtype):
